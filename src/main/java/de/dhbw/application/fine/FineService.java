7957a6bd4fd9d225b57b65d0d0be6e23f3ad6932
Finn Hannon <f.hannon@netze-bw.de>
2026-03-17 23:29:50 +0100

    feat: add FineService skeleton

--- src/main/java/de/dhbw/application/fine/FineService.java
+++ src/main/java/de/dhbw/application/fine/FineService.java
@@ -1,5 +1,59 @@
 package de.dhbw.application.fine;
 
+import de.dhbw.domain.fine.Fine;
+import de.dhbw.domain.fine.FineStatus;
+import de.dhbw.persistence.fine.FineRepository;
+
+import java.util.List;
+import java.util.UUID;
+
 public class FineService {
-    // empty skeleton
+    private final FineRepository fineRepository;
+
+    public FineService(
+        FineRepository fineRepository
+    ) {
+        this.fineRepository = fineRepository;
+    }
+
+    public void payFine(UUID fineId, double amount) {
+
+    }
+
+    public void waiveFine(UUID fineId, String note) {
+
+    }
+
+    public void createFine(UUID loanId, UUID userId) {
+
+    }
+
+    public List<Fine> getAllFines() {
+
+    }
+
+    public Fine getFineById(UUID fineId) {
+
+    }
+
+    public List<Fine> getFinesByUserId(UUID userId) {
+
+    }
+
+    public List<Fine> getFinesByStatus(FineStatus status) {
+
+    }
+
+    /**
+     * Generates a unique id for a fine.
+     *
+     * @return {UUID} fineId - the generated unique id.
+     */
+    private UUID generateFineId() {
+        UUID fineId;
+        do {
+            fineId = UUID.randomUUID();
+        } while (fineRepository.findById(fineId).isPresent());
+        return fineId;
+    }
 }
--- src/main/java/de/dhbw/persistence/fine/FineRepository.java
+++ src/main/java/de/dhbw/persistence/fine/FineRepository.java
@@ -2,17 +2,16 @@ package de.dhbw.persistence.fine;
 
 import de.dhbw.domain.fine.Fine;
 import de.dhbw.domain.fine.FineStatus;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface FineRepository {
     void save(Fine fine);
+    void update(Fine fine);
+    void delete(UUID id);
     Optional<Fine> findById(UUID id);
     List<Fine> findAll();
     List<Fine> findByUserId(UUID userId);
     List<Fine> findByStatus(FineStatus status);
-    void update(Fine fine);
-    void delete(UUID id);
 }
