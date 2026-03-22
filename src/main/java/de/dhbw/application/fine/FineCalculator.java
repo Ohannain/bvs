8a692a8cc0b010cc6068a6fc7de828a037a1fd4a
Finn Hannon <f.hannon@netze-bw.de>
2026-03-22 13:16:25 +0100

    fix: implemented missing calculateDaysSince method

--- src/main/java/de/dhbw/application/fine/FineCalculator.java
+++ src/main/java/de/dhbw/application/fine/FineCalculator.java
@@ -2,6 +2,10 @@ package de.dhbw.application.fine;
 
 import de.dhbw.domain.loan.Loan;
 import de.dhbw.domain.media.Media;
+import de.dhbw.util.Config;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class FineCalculator {
     public enum DamageLevel {
@@ -18,7 +22,7 @@ public class FineCalculator {
         if (loan == null) { return 0.0; }
         double fine = 0.0;
 
-        int daysBetween = calculateDaysSince();
+        int daysBetween = calculateDaysSince(loan.getDueDate());
 
         if (daysBetween > 0) { fine = daysBetween * Config.DEFAULT_FINE_RATE_PER_DAY; }
 
@@ -31,7 +35,7 @@ public class FineCalculator {
     public static double calculateLostItemFine(Media media) {
         if (media == null) { return 0.0; }
 
-        return media.replacementCost;
+        return media.getReplacementCost();
     }
 
     /**
@@ -39,11 +43,19 @@ public class FineCalculator {
      */
     public static double calculateDamageFine(Media media, DamageLevel damageLevel) {
         return switch (damageLevel) {
-            case MINOR -> media.replacementCost * 0.25;
-            case MODERATE -> media.replacementCost * 0.5;
-            case SEVERE -> media.replacementCost * 0.75;
-            case TOTAL -> media.replacementCost;
+            case MINOR -> media.getReplacementCost() * 0.25;
+            case MODERATE -> media.getReplacementCost() * 0.5;
+            case SEVERE -> media.getReplacementCost() * 0.75;
+            case TOTAL -> media.getReplacementCost();
             default -> 0;
         };
+    }
+    
+    /**
+     * calculateDaysSince calculates the amount of days since a fine was overdue
+     */
+    private static int calculateDaysSince(LocalDate date) {
+        LocalDate today = LocalDate.now();
+        return Math.toIntExact(ChronoUnit.DAYS.between(date, today));
     }
 }
