0d6b2cdac8017d2193605e64ad8d752998d7dec4
benjamin riedel <benjamin.riedel@mail.de>
2026-04-01 18:07:24 +0200

    reservation service and controller

--- src/main/java/de/dhbw/application/reservation/ReservationController.java
+++ src/main/java/de/dhbw/application/reservation/ReservationController.java
@@ -1,5 +1,47 @@
 package de.dhbw.application.reservation;
 
+import de.dhbw.domain.reservation.Reservation;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public class ReservationController {
-    // empty skeleton
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    public Reservation makeReservation(UUID userId, UUID mediaId) {
+        return reservationService.createReservation(userId, mediaId);
+    }
+
+    public void fulfillReservation(UUID reservationId) {
+        reservationService.fulfillReservation(reservationId);
+    }
+
+    public void cancelReservation(UUID reservationId) {
+        reservationService.cancelReservation(reservationId);
+    }
+
+    public List<Reservation> getUserReservations(UUID userId) {
+        return reservationService.getReservationsByUserId(userId);
+    }
+
+    public List<Reservation> getMediaReservations(UUID mediaId) {
+        return reservationService.getReservationsByMediaId(mediaId);
+    }
+
+    public List<Reservation> getActiveReservations() {
+        return reservationService.getActiveReservations();
+    }
+
+    public Optional<Reservation> getReservationDetails(UUID reservationId) {
+        return reservationService.getReservationById(reservationId);
+    }
+
+    public void checkAndExpireReservations() {
+        reservationService.checkExpiredReservations();
+    }
 }
